58faa32d4ec94c432049313cf7d39e0572ca9c3b
devonbahary <devonbahary@gmail.com>
2017-05-31 16:27:55 -0700

    Assignment 3 Routing and States | Completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,6 +22,10 @@
       .state('album', {
         url: '/album',
         templateUrl: '/templates/album.html'
+      })
+      .state('collection', {
+        url: '/collection',
+        templateUrl: '/templates/collection.html'
       });
   }
 
